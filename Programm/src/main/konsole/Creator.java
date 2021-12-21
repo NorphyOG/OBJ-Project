71d6d48c990eebff2446997741da958d0d939dce
NorphyOG <91422656+NorphyOG@users.noreply.github.com>
2021-12-21 11:29:56 +0100

    consol

--- Programm/src/main/konsole/Creator.java
+++ Programm/src/main/konsole/Creator.java
@@ -2,9 +2,7 @@ package main.konsole;
 
 import main.Function;
 import main.History;
-import main.personen.Arbeiter;
-import main.personen.Person;
-import main.personen.ZeitArbeiter;
+import main.personen.*;
 
 import java.time.LocalDate;
 import java.util.InputMismatchException;
@@ -31,7 +29,7 @@ public class Creator {
         int arbeitsZeitM = 0;
 
         //Manager
-        Person[] gewPersonenListe;
+        Person[] gewPersonenListe = new Person[99];
         double provision = 0;
 
         //GFührer
@@ -242,6 +240,9 @@ public class Creator {
 
         History history = new History();
         Function function = new Function();
+        PersonData personData = new PersonData();
+        Person person = null;
+
         switch (switchLevel) {
             case 0:
                 ff = false;
@@ -264,25 +265,41 @@ public class Creator {
                 }
 
                 ZeitArbeiter zeitArbeiter = new ZeitArbeiter(vorname, nachname, geschlecht, adresse, geb, eintritt, lohnM, arbeitsStunden, arbeitsZeitM);
-                zeitArbeiter.setLohnZeitArbeiter(zeitArbeiter);
-                function.setID(zeitArbeiter);
-                history.personChanged(zeitArbeiter, 1);
-                return zeitArbeiter;
+                personData.setup(zeitArbeiter, 0, 1);
+                person = zeitArbeiter;
                 break;
             case 1:
                 Arbeiter arbeiter = new Arbeiter(vorname, nachname, geschlecht, adresse, geb, eintritt, lohnM, arbeitsStunden);
-                arbeiter.setLohnArbeiter(arbeiter);
-                function.setID(arbeiter);
-                history.personChanged(arbeiter, 1);
-                return arbeiter;
+                personData.setup(arbeiter, 1, 1);
+                person = arbeiter;
                 break;
             case 2:
+
+                ff = false;
+                run = true;
+                while (run) {
+                    if (!ff) {
+                        overlay.setOption1("Provision eingeben (Formart: (1.23) = 23%)");
+                        overlay.o1ss(false);
+                    }
+                    try {
+                        provision = sc.nextInt();
+                        run = false;
+                        ff = false;
+                    } catch (InputMismatchException e) {
+                        overlay.setErr("Error (Keine gültige Eingabe)");
+                        overlay.o1ss(true);
+                        ff = true;
+                        sc.next();
+                    }
+                }
+
                 int angeworben = 0;
                 ff = false;
                 run = true;
                 while (run) {
                     if (!ff) {
-                        overlay.setOption1("Wie viel leute hat er angeworben?");
+                        overlay.setOption1("Wie viel leute hat er angeworben? (ID muss vorhanden sein) - (MAX 99)");
                         overlay.o1ss(false);
                     }
                     try {
@@ -297,11 +314,54 @@ public class Creator {
                     }
                 }
 
+                for (int i = 0; i < angeworben; i++) {
+                    String scanner = null;
+                    overlay.setOption1("ID der Person eingeben");
+                    overlay.o1ss(false);
+                    scanner = sc.next();
 
+                    for (Person[] p : function.getPersonArrayList()) {
+                        for (Person pr : p) {
+                            if (pr != null) {
+                                if (pr.getId().equals(scanner)) {
+                                    gewPersonenListe[i] = pr;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                Manager manager = new Manager(vorname, nachname, geschlecht, adresse, geb, eintritt, lohnM, arbeitsStunden, gewPersonenListe, provision);
+                personData.setup(manager, 2, 1);
+                person = manager;
                 break;
             case 3:
+                ff = false;
+                run = true;
+                while (run) {
+                    if (!ff) {
+                        overlay.setOption1("Zuschlag eingeben");
+                        overlay.o1ss(false);
+                    }
+                    try {
+                        zuschlag = sc.nextInt();
+                        run = false;
+                        ff = false;
+                    } catch (InputMismatchException e) {
+                        overlay.setErr("Error (Keine gültige Eingabe)");
+                        overlay.o1ss(true);
+                        ff = true;
+                        sc.next();
+                    }
+                }
+
+                Geschaeftsfuehrer geschaeftsfuehrer = new Geschaeftsfuehrer(vorname, nachname, geschlecht, adresse, geb, eintritt, lohnM, zuschlag, arbeitsStunden);
+                personData.setup(geschaeftsfuehrer, 3, 1);
+                person = geschaeftsfuehrer;
                 break;
         }
+
+        return person;
 
 
     }
