3094842b87bf528816d160a7db1d35d71e6bd51d
NorphyOG <91422656+NorphyOG@users.noreply.github.com>
2021-12-21 10:53:18 +0100

    personData

--- Programm/src/main/personen/PersonData.java
+++ Programm/src/main/personen/PersonData.java
@@ -7,12 +7,40 @@ public class PersonData {
 
     private boolean useDataPresent;
 
+    private Function function = new Function();
+    private History history = new History();
+
+    public void setup(Person person, int personLevel, int changedLevel) {
+        ZeitArbeiter zeitArbeiterUntil = null;
+        Arbeiter arbeiterUntil = null;
+        Manager managerUntil = null;
+        Geschaeftsfuehrer geschaeftsfuehrerUntil = null;
+
+        switch (personLevel) {
+            case 0 -> {
+                assert false;
+                zeitArbeiterUntil.setLohnZeitArbeiter((ZeitArbeiter) person);
+            }
+            case 1 -> {
+                assert false;
+                arbeiterUntil.setLohnArbeiter((Arbeiter) person);
+            }
+            case 2 -> {
+                assert false;
+                managerUntil.setLohnManager((Manager) person);
+            }
+            case 3 -> {
+                assert false;
+                geschaeftsfuehrerUntil.setLohnGeschäftsführer((Geschaeftsfuehrer) person);
+            }
+        }
+        function.setID(person);
+        history.personChanged(person, changedLevel);
+    }
 
     public boolean dataPresent() {
         //Data TODO
         if (useDataPresent) {
-            Function function = new Function();
-            History history = new History();
 
 
             Geschaeftsfuehrer geschaeftsfuehrer = new Geschaeftsfuehrer("Günter","");
