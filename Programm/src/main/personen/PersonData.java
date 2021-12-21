ad8ad49d437cbde30dbec5df8dab9b6a50fcee79
Ballduin <adrian.wahlhaeuser@gmx.de>
2021-12-21 10:46:19 +0100

    update

--- Programm/src/main/personen/PersonData.java
+++ Programm/src/main/personen/PersonData.java
@@ -1,5 +1,8 @@
 package main.personen;
 
+import main.Function;
+import main.History;
+
 public class PersonData {
 
     private boolean useDataPresent;
@@ -8,6 +11,26 @@ public class PersonData {
     public boolean dataPresent() {
         //Data TODO
         if (useDataPresent) {
+            Function function = new Function();
+            History history = new History();
+
+
+            Geschaeftsfuehrer geschaeftsfuehrer = new Geschaeftsfuehrer("Günter","");
+            geschaeftsfuehrer.setLohnGeschäftsführer(geschaeftsfuehrer);
+            function.setID(geschaeftsfuehrer);
+            history.personChanged(geschaeftsfuehrer, 1);
+
+            Manager manager1 = new Manager();
+            Manager manager2 = new Manager();
+
+            Arbeiter arbeiter1 = new Arbeiter();
+            Arbeiter arbeiter2 = new Arbeiter();
+            Arbeiter arbeiter3 = new Arbeiter();
+            Arbeiter arbeiter1 = new Arbeiter();
+            Arbeiter arbeiter2 = new Arbeiter();
+            Arbeiter arbeiter3 = new Arbeiter();
+
+            ZeitArbeiter zeitArbeiter1 = new ZeitArbeiter();
 
         }
         return useDataPresent;
